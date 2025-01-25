9fcdf1e03bce0e116b75c6609631fd1e85881882
Ecchibitionist <ciapa@lewd.wtf>
2025-01-25 16:55:12 +0100

    Implement overworld elements like the shop, trees etc

--- tiled/overworld.tsx
+++ tiled/overworld.tsx
@@ -1,40 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Overworld" tilewidth="16" tileheight="16" tilecount="12" columns="0">
+<tileset version="1.10" tiledversion="1.11.1" name="Overworld" tilewidth="64" tileheight="64" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
-  <image source="../assets/sprites/water_wave.png" width="16" height="16"/>
+  <image source="sprites/water_wave.png" width="16" height="16"/>
  </tile>
  <tile id="3">
-  <image source="../assets/sprites/ground.png" width="16" height="16"/>
+  <image source="sprites/ground.png" width="16" height="16"/>
  </tile>
  <tile id="20">
-  <image source="../assets/sprites/path1.png" width="16" height="16"/>
+  <image source="sprites/path1.png" width="16" height="16"/>
  </tile>
  <tile id="21">
-  <image source="../assets/sprites/path2.png" width="16" height="16"/>
+  <image source="sprites/path2.png" width="16" height="16"/>
  </tile>
  <tile id="22">
-  <image source="../assets/sprites/path3.png" width="16" height="16"/>
+  <image source="sprites/path3.png" width="16" height="16"/>
  </tile>
  <tile id="23">
-  <image source="../assets/sprites/path4.png" width="16" height="16"/>
+  <image source="sprites/path4.png" width="16" height="16"/>
  </tile>
  <tile id="24">
-  <image source="../assets/sprites/path5.png" width="16" height="16"/>
+  <image source="sprites/path5.png" width="16" height="16"/>
  </tile>
  <tile id="25">
-  <image source="../assets/sprites/path6.png" width="16" height="16"/>
+  <image source="sprites/path6.png" width="16" height="16"/>
  </tile>
  <tile id="26">
-  <image source="../assets/sprites/path7.png" width="16" height="16"/>
+  <image source="sprites/path7.png" width="16" height="16"/>
  </tile>
  <tile id="27">
-  <image source="../assets/sprites/path8.png" width="16" height="16"/>
+  <image source="sprites/path8.png" width="16" height="16"/>
  </tile>
  <tile id="28">
-  <image source="../assets/sprites/path9.png" width="16" height="16"/>
+  <image source="sprites/path9.png" width="16" height="16"/>
  </tile>
  <tile id="29">
-  <image source="../assets/sprites/path10.png" width="16" height="16"/>
+  <image source="sprites/path10.png" width="16" height="16"/>
+ </tile>
+ <tile id="30">
+  <image source="sprites/house.png" width="64" height="64"/>
+ </tile>
+ <tile id="32">
+  <image source="sprites/tree_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="33">
+  <image source="sprites/rock_1.png" width="16" height="16"/>
  </tile>
 </tileset>
